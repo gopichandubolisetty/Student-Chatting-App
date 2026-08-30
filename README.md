948109e9f691ec54e2f4e16e0b5f67699eb749a2
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-08-30 10:09:08 +0530

    Remove old codebase to start fresh

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the platform where student can discuss each other 
--- server/models/Course.js
+++
@@ -1,8 +0,0 @@
-const courseSchema = new mongoose.Schema({
-    subjectName :{type:String,required:true},
-    slots:[{
-        slotName:String,
-        facultyName:String,
-        roomId :{type:String,unique:true}
-    }]
-});
